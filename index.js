f9ea943f618f6a534d1725fd6bb114a18d727ff8
TommmyKelly <tommy_kelly@icloud.com>
2020-07-24 18:25:08 +0100

    new3

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/',(req, res)=> {
  res.send('hello')
 })
 
+app.get('/login',(req, res)=> {
+    res.send('login')
+   })
+
 app.listen(PORT,()=>{
     console.log(`listening on port ${PORT}`)
 })
