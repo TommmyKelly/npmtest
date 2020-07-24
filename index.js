fd4a5c41f9c1fd62a3202bf3a88fed1eeeb2c416
TommmyKelly <tommy_kelly@icloud.com>
2020-07-24 18:07:36 +0100

    commit new

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
 
-
+const PORT = process.env.PORT 
 app.get('/',(req, res)=> {
  res.send('hello')
 })
 
-app.listen(3000)
+app.listen(PORT)
